056d6ce46ba67e442325d930aea3e3832d8451fa
Aaron <aaron.l.france@gmail.com>
2013-03-25 21:11:48 +0100

    Added a CreateRepositoryResponse to marshal the response from the CreateRepository function into.

--- gitlabcli.go
+++ gitlabcli.go
@@ -11,6 +11,40 @@ import (
 	"path/filepath"
 )
 
+type owner struct {
+	ID        int64  `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Name      string `json:"name"`
+	State     string `json:"state"`
+	CreatedAt string `json:"created_at"`
+}
+type namespace struct {
+	CreatedAt   string  `json:"created_at"`
+	Description *string `json:"description"`
+	ID          int64   `json:"id"`
+	Name        string  `json:"name"`
+	OwnerID     int64   `json:"owner_id"`
+	Path        string  `json:"path"`
+	UpdatedAt   string  `json:"updated_at"`
+}
+
+type CreateRepositoryResponse struct {
+	ID                   int64     `json:"id"`
+	Description          *string   `json:"description"`
+	DefaultBranch        *string   `json:"default_branch"`
+	Owner                owner     `json:"owner"`
+	Public               bool      `json:"public"`
+	Path                 string    `json:"path"`
+	PathWithNS           string    `json:path_with_namespace"`
+	IssuesEnabled        bool      `json:"issues_enabled"`
+	MergeRequestsEnabled bool      `json:"merge_requests_enabled"`
+	WallEnabled          bool      `json:"wall_enabled"`
+	WikiEnabled          bool      `json:"wiki_enabled"`
+	CreatedAt            string    `json:"created_at"`
+	Namespace            namespace `json:"namespace"`
+}
+
 func init() {
 	home := os.Getenv("HOME")
 	if home == "" {
@@ -30,7 +64,8 @@ func init() {
 	}
 }
 
-func CreateRepository(r string, extra *map[string]string) error {
+/* Will create a new repository using the global conf object. */
+func CreateRepository(r string, extra *map[string]string) (*CreateRepositoryResponse, error) {
 	path := fmt.Sprintf(conf.Endpoint+"projects?name=%s&", r)
 	vals := url.Values{}
 	if extra != nil {
@@ -45,20 +80,39 @@ func CreateRepository(r string, extra *map[string]string) error {
 	)
 	c := http.Client{}
 	if err != nil {
-		return err
+		return nil, err
 	}
 	req.Header.Add("PRIVATE-TOKEN", conf.APIKey)
 	resp, err := c.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	fmt.Println(string(body))
-	return nil
+	switch resp.StatusCode {
+	case http.StatusCreated:
+		crr := &CreateRepositoryResponse{}
+		err = json.Unmarshal(body, crr)
+		if err != nil {
+			return nil, err
+		}
+		return crr, nil
+	default:
+		type Message struct {
+			M string `json:"message"`
+		}
+		var msg Message
+		err = json.Unmarshal(body, &msg)
+		if err != nil {
+			return nil, err
+		}
+		return nil, errors.New(msg.M)
+	}
+	panic("Unreachable!")
+
 }
 
 type ConfigFile struct {
