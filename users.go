2aa4c03885b6fac985d5d9545dc8c15cf651c0d2
Aaron France <aaron.france@hp.com>
2013-04-06 08:44:22 -0400

    Added ListUsers and changed the User struct to deal with null values.

--- users.go
+++ users.go
@@ -9,16 +9,37 @@ import (
 )
 
 type User struct {
-	Email        string `json:"email"`
-	Username     string `json:"username"`
-	Password     string `json:"password"`
-	Skype        string `json:"skype"`
-	LinkedIn     string `json:"linkedin"`
-	Twitter      string `json:"twitter"`
-	ProjectLimit int64  `json:"projects_limit"`
-	ExternalUID  string `json:"external_UID"`
-	Provider     string `json:"provider"`
-	Bio          string `json:"bio"`
+	ID           int64   `json:"id"`
+	Email        string  `json:"email"`
+	Name         string  `json:"name"`
+	Blocked      bool    `json:"blocked"`
+	CreatedAt    string  `json:"create_at"`
+	Username     string  `json:"username"`
+	Password     string  `json:"password"`
+	Skype        string  `json:"skype"`
+	LinkedIn     string  `json:"linkedin"`
+	Twitter      string  `json:"twitter"`
+	ProjectLimit int64   `json:"projects_limit"`
+	ExternalUID  *string `json:"external_UID"`
+	Provider     *string `json:"provider"`
+	Bio          *string `json:"bio"`
+	DarkScheme   bool    `json:"dark_scheme"`
+	ThemeID      int64   `json:"theme_id"`
+}
+
+func ListUsers(conf ConfigFile) ([]User, error) {
+	resp, err := http.Get(
+		fmt.Sprintf("%s/%s?private_token=%s", conf.Endpoint, "users", conf.APIKey),
+	)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	users := []User{}
+	return users, json.Unmarshal(body, &users)
 }
 
 func CreateUser(conf ConfigFile, u User) error {
