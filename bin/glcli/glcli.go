b4dfa8543b54b0954c18746997b3bcfd028762bf
Aaron <aaron.l.france@gmail.com>
2013-04-08 00:26:58 +0200

    Moved all handling of the various modes into handler functions to aid in navigatability.

--- bin/glcli/glcli.go
+++ bin/glcli/glcli.go
@@ -43,111 +43,137 @@ func init() {
 		conf = c
 	}
 }
-func main() {
-	if *Create != "" {
-		crr, err := webhooks.CreateRepository(conf, *Create, nil)
-		if err != nil {
-			fmt.Println("Error creating repository: " + err.Error())
+
+func HandleCreate() {
+	crr, err := webhooks.CreateRepository(conf, *Create, nil)
+	if err != nil {
+		fmt.Println("Error creating repository: " + err.Error())
+		return
+	}
+	fmt.Printf("New repository created with ID: %d\n", crr.ID)
+}
+func HandleInit() {
+	/* Run the Git command to create a new repository locally */
+	cmd := exec.Command("git", "init", *Init)
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	crr, err := webhooks.CreateRepository(conf, *Init, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	/* Move into the new sub directory */
+	err = os.Chdir(*Init)
+	if err != nil {
+		log.Fatal(err)
+	}
+	/* Add the remote as the origin of the new repository */
+	cmd = exec.Command(
+		"git", "remote", "add", "origin", conf.GitURL+crr.PathWithNS,
+	)
+	cmd.Stdout = os.Stdout
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+
+}
+
+func HandleUser() {
+	for field, errmsg := range map[string]string{
+		*Email:    "Missing e-mail.",
+		*Username: "Missing username.",
+		*Password: "Missing password.",
+	} {
+		if field == "" {
+			fmt.Println(errmsg)
 			return
 		}
-		fmt.Printf("New repository created with ID: %d\n", crr.ID)
-		return
 	}
-	if *Init != "" {
-		/* Run the Git command to create a new repository locally */
-		cmd := exec.Command("git", "init", *Init)
-		cmd.Stdout = os.Stdout
-		err := cmd.Run()
-		if err != nil {
-			log.Fatal(err)
-		}
-		crr, err := webhooks.CreateRepository(conf, *Init, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-		/* Move into the new sub directory */
-		err = os.Chdir(*Init)
-		if err != nil {
-			log.Fatal(err)
-		}
-		/* Add the remote as the origin of the new repository */
-		cmd = exec.Command(
-			"git", "remote", "add", "origin", conf.GitURL+crr.PathWithNS,
-		)
-		cmd.Stdout = os.Stdout
-		err = cmd.Run()
-		if err != nil {
-			log.Fatal(err)
-		}
-		return
+	user := webhooks.User{
+		Email:        *Email,
+		Name:         *Name,
+		Username:     *Username,
+		Password:     *Password,
+		Skype:        *Skype,
+		LinkedIn:     *LinkedIn,
+		Twitter:      *Twitter,
+		ProjectLimit: *ProjectLimit,
 	}
-	if *User {
-		for field, errmsg := range map[string]string{
-			*Email:    "Missing e-mail.",
-			*Username: "Missing username.",
-			*Password: "Missing password.",
-		} {
-			if field == "" {
-				fmt.Println(errmsg)
-				return
-			}
-		}
-		user := webhooks.User{
-			Email:        *Email,
-			Name:         *Name,
-			Username:     *Username,
-			Password:     *Password,
-			Skype:        *Skype,
-			LinkedIn:     *LinkedIn,
-			Twitter:      *Twitter,
-			ProjectLimit: *ProjectLimit,
-		}
-		fmt.Println(webhooks.CreateUser(conf, user))
-		return
-	}
+	fmt.Println(webhooks.CreateUser(conf, user))
 
-	if *DelUser && *DelUserId != -1 {
-		fmt.Println(webhooks.DeleteUser(conf, *DelUserId))
+}
+
+func HandleDelete() {
+	fmt.Println(webhooks.DeleteUser(conf, *DelUserId))
+}
+
+func HandleListUsers() {
+	users, err := webhooks.ListUsers(conf)
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
-
-	if *ListUsers {
-		users, err := webhooks.ListUsers(conf)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		for _, user := range users {
-			fmt.Println(fmt.Sprintf(
-				`ID: %d
+	for _, user := range users {
+		fmt.Println(fmt.Sprintf(
+			`ID: %d
 Email: %s
 Name: %s
 Username: %s
 -------------------`, user.ID, user.Email, user.Name, user.Username,
-			))
-		}
+		))
 	}
+}
 
-	if *ListProjects {
-		projects, err := webhooks.ListProjects(conf)
-		if err != nil {
-			fmt.Println(err)
-			return
+func HandleListProjects() {
+	projects, err := webhooks.ListProjects(conf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, project := range projects {
+		desc := "<empty>"
+		if project.Description != nil {
+			desc = *project.Description
 		}
-		for _, project := range projects {
-			desc := "<empty>"
-			if project.Description != nil {
-				desc = *project.Description
-			}
-			fmt.Println(fmt.Sprintf(
-				`ID: %d
+		fmt.Println(fmt.Sprintf(
+			`ID: %d
 Name: %s
 Description: %s
 Owner: %s
 Path: %s
 -------------------`,
-				project.ID, project.Name, desc, project.Owner.Username, project.Path,
-			))
-		}
+			project.ID, project.Name, desc, project.Owner.Username, project.Path,
+		))
+	}
+
+}
+
+func main() {
+	if *Create != "" {
+		HandleCreate()
+	}
+
+	if *Init != "" {
+		HandleInit()
+	}
+
+	if *User {
+		HandleUser()
+	}
+
+	if *DelUser && *DelUserId != -1 {
+		HandleDelete()
+	}
+
+	if *ListUsers {
+		HandleListUsers()
+	}
+
+	if *ListProjects {
+		HandleListProjects()
 	}
 }
